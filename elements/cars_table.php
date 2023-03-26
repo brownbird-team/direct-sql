0bed66569a8324eab4292b583259669663ce490c
rdobovic <roko@brownbird.eu>
2023-03-26 23:41:49 +0200

    Manje izmjene u stilovima

--- elements/cars_table.php
+++ elements/cars_table.php
@@ -6,33 +6,34 @@ $car_res = $conn -> query('SELECT * FROM cars');
 $fields = $car_res -> fetch_fields();
 
 ?>
-
-<table>
-    <tr>
-        <?php foreach ($fields as $field)
-             echo '<th>'. $field -> name .'</th>';
-        ?>
-    </tr>
-    
-    <?php while ($car = $car_res -> fetch_assoc()) {
-    
-        echo '<tr>';
-
-        for ($i = 0; $i < count($fields); $i++) {
-            if ($fields[$i] -> type == 252)
-                echo '<td><img alt="image" src="data:image/jpg;charset=utf8;base64,'. base64_encode($car[$fields[$i] -> name]) .'"></td>';
-            else
-                echo '<td>'. $car[$fields[$i] -> name] .'</td>';
-        }
-
-        echo '</tr>';
-        
-    } ?>
-
-    <?php if ($car_res -> num_rows == 0): ?>
+<div class="table-container">
+    <table>
         <tr>
-            <td colspan="<?php echo $car_res -> field_count ?>">No data to display</td>
+            <?php foreach ($fields as $field)
+                echo '<th>'. $field -> name .'</th>';
+            ?>
         </tr>
-    <?php endif; ?>
-</table>
+        
+        <?php while ($car = $car_res -> fetch_assoc()) {
+        
+            echo '<tr>';
+
+            for ($i = 0; $i < count($fields); $i++) {
+                if ($fields[$i] -> type == 252)
+                    echo '<td><img alt="image" src="data:image/jpg;charset=utf8;base64,'. base64_encode($car[$fields[$i] -> name]) .'"></td>';
+                else
+                    echo '<td>'. $car[$fields[$i] -> name] .'</td>';
+            }
+
+            echo '</tr>';
+            
+        } ?>
+
+        <?php if ($car_res -> num_rows == 0): ?>
+            <tr>
+                <td colspan="<?php echo $car_res -> field_count ?>">No data to display</td>
+            </tr>
+        <?php endif; ?>
+    </table>
+</div>
 
--- index.php
+++ index.php
@@ -7,13 +7,17 @@
     <link rel="stylesheet" href="stlyes/main.css">
     <title>Hello Cars</title>
     <link rel='stylesheet' type='text/css' href='styles/main.css'/>
-    <link rel='stylesheet' type='text/css' href='styles/responsive.css'/>
     <link rel='stylesheet' type='text/css' href='styles/insert_form.css'/>
     <link rel='stylesheet' type='text/css' href='styles/table.css'/>
+    <link rel='stylesheet' type='text/css' href='styles/nav_bar.css'/>
+    <link rel='stylesheet' type='text/css' href='styles/responsive.css'/>
 </head>
 <body>
-    <iframe src="navigation_bar.php" width="100%" frameborder="0"></iframe>
-    <img src="images/logo.png" class="logo">
+    <!-- <iframe src="navigation_bar.php" width="100%" frameborder="0"></iframe> -->
+
+    <?php require __DIR__ . '/elements/navigation_bar.php' ?>
+
+    <!-- <img src="images/logo.png" class="imglogo"> -->
     <div class="container">
         <h1>Cars Database</h1>
 
