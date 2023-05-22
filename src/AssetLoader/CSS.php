431565905a228c494896787eab0fdd189a5ce4a6
rdobovic <roko@brownbird.eu>
2023-05-22 10:12:47 +0200

    Dodajem funkciju za eksterne linkove CSS

--- src/AssetLoader/CSS.php
+++ src/AssetLoader/CSS.php
@@ -2,16 +2,26 @@
 
 namespace AssetLoader;
 
+const CSS_DIRECTORY_PATH = './public/css/';
+
 class CSS {
     private static $files_array = [];
+    private static $external_files_array = [];
 
     public static function include_file($file_path) {
         self::$files_array[] = $file_path;
     }
 
+    public static function include_external_file($link) {
+        self::$external_files_array[] = $link;
+    }
+
     public static function print_links() {
+        foreach (self::$external_files_array as $file) {
+            echo '<link type="text/css" rel="stylesheet" href="'. $file .'" />';
+        }
         foreach (self::$files_array as $file) {
-            echo '<link type="text/css" rel="stylesheet" href="./public/css/'. $file .'" />';
+            echo '<link type="text/css" rel="stylesheet" href="'. CSS_DIRECTORY_PATH . $file .'" />';
         }
     }
 }
